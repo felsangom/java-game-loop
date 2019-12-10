d514ea650182acbc48cdf479e1efde448236c51a
Felipe Gomes <felipe.gomes@aviorlogistica.com.br>
2019-12-10 12:51:06 -0300

    Commit inicial

---
+++ src/GameLoop.java
@@ -0,0 +1,60 @@
+public class GameLoop implements Runnable {
+
+	private Thread thread;
+	private boolean isRunning = false;
+
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		isRunning = false;
+
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void tick() {
+		
+	}
+
+	public void render() {
+		
+	}
+	
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			if (delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+
+			if (System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer += 1000;
+			}
+		}
+
+		stop();
+	}
+}
